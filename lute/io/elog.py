5e50fca2bb3ebe65a420b41ce24ec362eca7cff5
gadorlhiac <gadorlhiac@gmail.com>
2024-02-09 09:47:20 -0800

    ENH Add more eLog func skeletons. Begin moving auth selection to . --Currently broken--

--- lute/io/elog.py
+++ lute/io/elog.py
@@ -3,31 +3,48 @@
 Make use of various eLog API endpoint to retrieve information or post results.
 
 Functions:
-    get_elog_auth(exp: str): Return an authorization object to interact with
+    get_elog_opr_auth(exp: str): Return an authorization object to interact with
         eLog API as an opr account for the hutch where `exp` was conducted.
-    get_elog_http_request(url: str, request_type: str, **params): Make an HTTP
+
+    get_kerberos_auth(): Return the authorization headers for the user account
+        submitting the job.
+
+    elog_http_request(url: str, request_type: str, **params): Make an HTTP
         request to the API endpoint at `url`.
+
     format_file_for_post(in_file: Union[str, tuple, list]): Prepare files
         according to the specification needed to add them as attachments to eLog
         posts.
+
     post_elog_message(exp: str, msg: str, tag: Optional[str],
                       title: Optional[str],
                       in_files: List[Union[str, tuple, list]],
                       auth: Optional[Union[HTTPBasicAuth, Dict]] = None)
         Post a message to the eLog.
-    post_elog_runtable(exp: str, run: int, data: Dict[str, Any],
+
+    post_elog_run_status(data: Dict[str, Union[str, int, float]],
+                         update_url: Optional[str] = None)
+        Post a run status to the summary section on the Workflows>Control tab.
+
+    post_elog_run_table(exp: str, run: int, data: Dict[str, Any],
                        auth: Optional[Union[HTTPBasicAuth, Dict]] = None)
         Update run table in the eLog.
+
     get_elog_runs_by_tag(exp: str, tag: str,
                          auth: Optional[Union[HTTPBasicAuth, Dict]] = None)
         Return a list of runs with a specific tag.
+
+    get_elog_params_by_run(exp: str, params: List[str], runs: Optional[List[int]])
+        Retrieve the requested parameters by run. If no run is provided, retrieve
+        the requested parameters for all runs.
 """
 
 __all__ = [
     "post_elog_message",
-    "post_elog_runtable",
+    "post_elog_run_table",
     "get_elog_runs_by_tag",
-    "post_run_status",
+    "post_elog_run_status",
+    "get_elog_params_by_run",
 ]
 __author__ = "Gabriel Dorlhiac"
 
@@ -40,39 +57,149 @@ import os
 from typing import Any, Dict, Optional, List, Union, Tuple
 from io import BufferedReader
 
-BASE_URL: str = "https://pswww.slac.stanford.edu/ws-auth/lgbk/lgbk"
+from krtc import KerberosTicket
 
 
-def get_elog_auth(exp: str) -> Union[HTTPBasicAuth, Dict]:
+def get_elog_workflows(exp: str) -> Dict[str, str]:
+    """Get the current workflow definitions for an experiment.
+
+    Returns:
+        defns (Dict[str, str]): A dictionary of workflow definitions.
+    """
+    raise NotImplementedError
+
+
+def post_elog_workflow(
+    exp: str,
+    name: str,
+    executable: str,
+    wf_params: str,
+    *,
+    trigger: str = "run_end",
+    location: str = "S3DF",
+    **trig_args: str,
+) -> None:
+    """Create a new eLog workflow, or update an existing one.
+
+    The workflow will run a specific executable as a batch job when the
+    specified trigger occurs. The precise arguments may vary depending on the
+    selected trigger type.
+
+    Args:
+        name (str): An identifying name for the workflow. E.g. "process data"
+
+        executable (str): Full path to the executable to be run.
+
+        wf_params (str): All command-line parameters for the executable as a string.
+
+        trigger (str): When to trigger execution of the specified executable.
+            One of:
+                - 'manual': Must be manually triggered. No automatic processing.
+                - 'run_start': Execute immediately if a new run begins.
+                - 'run_end': As soon as a run ends.
+                - 'param_is': As soon as a parameter has a specific value for a run.
+
+        location (str): Where to submit the job. S3DF or NERSC.
+
+        **trig_args (str): Arguments required for a specific trigger type.
+            trigger='param_is' - 2 Arguments
+                trig_param (str): Name of the parameter to watch for.
+                trig_param_val (str): Value the parameter should have to trigger.
+    """
+    endpoint: str = f"{exp}/ws/create_update_workflow_def"
+    trig_map: Dict[str, str] = {
+        "manual": "MANUAL",
+        "run_start": "START_OF_RUN",
+        "run_end": "END_OF_RUN",
+        "param_is": "RUN_PARAM_IS_VALUE",
+    }
+    if trigger not in trig_map.keys():
+        raise NotImplementedError(
+            f"Cannot create workflow with trigger type: {trigger}"
+        )
+    wf_defn: Dict[str, str] = {
+        "name": name,
+        "executable": executable,
+        "parameters": wf_params,
+        "trigger": trig_map[trigger],
+        "location": location,
+    }
+    if trigger == "param_is":
+        if "trig_param" not in trig_args or "trig_param_val" not in trig_args:
+            raise RuntimeError(
+                "Trigger type 'param_is' requires: 'trig_param' and 'trig_param_val' arguments"
+            )
+        wf_defn.update(
+            {
+                "run_param_name": trig_args["trig_param"],
+                "run_param_val": trig_args["trig_param_val"],
+            }
+        )
+    post_params: Dict[str, Dict[str, str]] = {"json": wf_defn}
+    status_code, resp_msg, _ = elog_http_request(
+        exp, endpoint=endpoint, request_type="POST", **post_params
+    )
+
+
+def get_elog_auth(exp: str) -> Union[HTTPBasicAuth, Dict[str, str]]:
+    """Determine the appropriate auth method depending on experiment state.
+
+    Returns:
+        auth (HTTPBasicAuth | Dict[str, str]): Depending on whether an experiment
+            is active/live, returns authorization for the hutch operator account
+            or the current user submitting a job.
+    """
+    return get_elog_opr_auth(exp)
+
+
+def get_elog_opr_auth(exp: str) -> HTTPBasicAuth:
     """Produce authentication for the "opr" user associated to an experiment.
 
-    Will return different authentication methods depending on whether the
-    requested experiment is active or not.
+    This method uses basic authentication using username and password.
 
     Args:
         exp (str): Name of the experiment to produce authentication for.
 
     Returns:
-        auth (HTTPBasicAuth | Dict): HTTPBasicAuth for an active experiment
-            based on username and password, or Kerberos headers for an inactive
-            experiment.
+        auth (HTTPBasicAuth): HTTPBasicAuth for an active experiment based on
+            username and password for the associated operator account.
     """
     # NEED TO FILL IN
     return HTTPBasicAuth("fake", "fake") or {}
 
 
+def get_kerberos_auth() -> Dict[str, str]:
+    """Returns Kerberos authorization key.
+
+    This functions returns authorization for the USER account submitting jobs.
+    It assumes that `kinit` has been run.
+
+    Returns:
+        auth (Dict[str, str]): Dictionary containing Kerberos authorization key.
+    """
+    return KerberosTicket("HTTP@pswww.slac.stanford.edu").getAuthHeaders()
+
+
 def elog_http_request(
-    url: str, request_type: str, **params
+    exp: str, endpoint: str, request_type: str, **params
 ) -> Tuple[int, str, Optional[Any]]:
     """Make an HTTP request to the eLog.
 
+    This method will determine the proper authorization method and update the
+    passed parameters appropriately. Functions implementing specific endpoint
+    functionality and calling this function should only pass the necessary
+    endpoint-specific parameters and not include the authorization objects.
+
     Args:
-        url (str): eLog API endpoint.
+        exp (str): Experiment.
+
+        endpoint (str): eLog API endpoint.
 
         request_type (str): Type of request to make. Recognized options: POST or
             GET.
-        **params (Dict): ALL parameters to pass with the HTTP request! Differs
-            depending on the API endpoint.
+
+        **params (Dict): Endpoint parameters to pass with the HTTP request!
+            Differs depending on the API endpoint. Do not include auth objects.
 
     Returns:
         status_code (int): Response status code. Can be checked for errors.
@@ -82,6 +209,17 @@ def elog_http_request(
         value (Optional[Any]): For GET requests ONLY, return the requested
             information.
     """
+    auth: Union[HTTPBasicAuth, Dict[str, str]] = get_elog_auth(exp)
+    base_url: str
+    if isinstance(auth, HTTPBasicAuth):
+        params.update({"auth": auth})
+        base_url = "https://pswww.slac.stanford.edu/ws-auth/lgbk/lgbk"
+    elif isinstance(auth, dict):
+        params.update({"headers": auth})
+        base_url = "https://pswww.slac.stanford.edu/ws-kerb/lgbk/lgbk"
+
+    url: str = f"{base_url}/{endpoint}"
+
     if request_type.upper() == "POST":
         resp: requests.models.Response = requests.post(url, **params)
     elif request_type.upper() == "GET":
@@ -126,7 +264,7 @@ def format_file_for_post(
     return out_file
 
 
-def post_run_status(
+def post_elog_run_status(
     data: Dict[str, Union[str, int, float]], update_url: Optional[str] = None
 ) -> None:
     """Post a summary to the status/report section of a specific run.
@@ -200,8 +338,9 @@ def post_elog_message(
     if title:
         post["log_title"] = title
 
-    auth: Union[HTTPBasicAuth, Dict] = auth or get_elog_auth(exp)
+    auth: Union[HTTPBasicAuth, Dict] = auth or get_elog_opr_auth(exp)
     post_url: str = f"{BASE_URL}/{exp}/ws/new_elog_entry"
+    # post_endpoint: str = f"{exp}/ws/new_elog_entry"
 
     params: Dict[str, Any] = {"data": post}
 
@@ -222,7 +361,7 @@ def post_elog_message(
     # NEED to handle/propagate errors...
 
 
-def post_elog_runtable(
+def post_elog_run_table(
     exp: str,
     run: int,
     data: Dict[str, Any],
@@ -247,6 +386,7 @@ def post_elog_runtable(
             return an error message.
     """
     table_url: str = f"{BASE_URL}/run_control/{exp}/ws/add_run_params"
+    # endpoint: str = f"run_control/{exp}/ws/add_run_params"
     auth: Union[HTTPBasicAuth, Dict] = auth or get_elog_auth(exp)
 
     params: Dict[str, Any] = {"params": {"run_num": run}, "json": data}
@@ -256,7 +396,9 @@ def post_elog_runtable(
     elif isinstance(auth, dict):
         params.update({"headers": auth})
 
-    status_code, resp_msg, _ = elog_http_request(url=table_url, request_type="POST")
+    status_code, resp_msg, _ = elog_http_request(
+        exp, url=table_url, request_type="POST", **params
+    )
 
     if resp_msg != "SUCCESS":
         return resp_msg
@@ -292,3 +434,18 @@ def get_elog_runs_by_tag(
         tagged_runs = []
 
     return tagged_runs
+
+
+def get_elog_params_by_run(
+    exp: str, params: List[str], runs: Optional[List[int]] = None
+) -> Dict[str, str]:
+    """Retrieve requested parameters by run or for all runs.
+
+    Args:
+        exp (str): Experiment to retrieve parameters for.
+
+        params (List[str]): A list of parameters to retrieve. These can be any
+            parameter recorded in the eLog (PVs, parameters posted by other
+            Tasks, etc.)
+    """
+    ...
