7b32d5e178ba7e4f76ebbff6dbaa8978708c564a
gadorlhiac <gadorlhiac@gmail.com>
2024-04-15 14:36:24 -0700

    ENH Launch wrapper accounts for partition and account without hard coding

--- launch_scripts/submit_launch_airflow.sh
+++ launch_scripts/submit_launch_airflow.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+
+# Need to capture partition and account for SLURM
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        --partition=*)
+            PARTITION="${1#*=}"
+            shift
+            ;;
+        --account=*)
+            ACCOUNT="${1#*=}"
+            shift
+            ;;
+        *)
+            POS+=("$1")
+            shift
+            ;;
+    esac
+done
+set -- "${POS[@]}"
+
 CMD="${@}"
+CMD="${CMD} --partition=${PARTITION} --account=${ACCOUNT}"
 echo $CMD
 CMD="/sdf/group/lcls/ds/tools/lute/lute_launcher ${CMD}"
-SLURM_ARGS="--partition=milano --account=lcls:data --ntasks=1"
+SLURM_ARGS="--partition=${PARTITION} --account=${ACCOUNT} --ntasks=1"
 echo "Running ${CMD} with ${SLURM_ARGS}"
 sbatch $SLURM_ARGS --wrap "${CMD}"
