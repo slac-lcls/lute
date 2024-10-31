5859d7c27052561536a2305ffa811352709d67c1
LouConreux <louis.conreux1@gmail.com>
2024-10-31 12:28:56 -0700

    [edit] Omit pyFAI INFO related messages for clarity

--- lute/execution/logging.py
+++ lute/execution/logging.py
@@ -60,10 +60,6 @@ def get_logger(name: str) -> logging.Logger:
             other_logger, logging.PlaceHolder
         ):
             other_logger.disabled = True
-            other_logger.propagate = False
-
-            for handler in other_logger.handlers:
-                handler.disabled = True
         elif "numpy" in other_name and not isinstance(
             other_logger, logging.PlaceHolder
         ):
