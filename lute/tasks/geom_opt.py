4f7f7ce5e3c13de3b08518aa1ab5d7249f48df00
Autoformatter <actions@github.com>
2024-11-15 22:33:53 +0000

    Auto-commit black formatting

--- lute/tasks/geom_opt.py
+++ lute/tasks/geom_opt.py
@@ -496,9 +496,15 @@ class BayesGeomOpt:
                 ai = sg.geometry_refinement
             if label is None:
                 label = sg.label
-        ax.imshow(powder.T, origin="lower", cmap="viridis", vmin=np.min(powder), vmax=self.Imin)
-        cbar = plt.colorbar(ax=ax, orientation='vertical')
-        cbar.set_label('Intensity')
+        ax.imshow(
+            powder.T,
+            origin="lower",
+            cmap="viridis",
+            vmin=np.min(powder),
+            vmax=self.Imin,
+        )
+        cbar = plt.colorbar(ax=ax, orientation="vertical")
+        cbar.set_label("Intensity")
         if ai is not None and cp.calibrant is not None:
             tth = cp.calibrant.get_2th()
             ttha = ai.twoThetaArray()
