44a3aefa9c4b1860f8f162362118d85c60c93aa4
Autoformatter <actions@github.com>
2024-12-01 07:48:05 +0000

    Auto-commit black formatting

--- lute/tasks/geom_opt.py
+++ lute/tasks/geom_opt.py
@@ -934,14 +934,17 @@ class OptimizePyFAIGeometry(Task):
                         if self._task_parameters.det_type == "Rayonix":
                             powder: npt.NDArray[np.float64] = h5[
                                 f"Sums/{self._task_parameters.det_type}_calib_skipFirst_max"
-                            ][()]   
+                            ][()]
                         powder: npt.NDArray[np.float64] = h5[
                             f"Sums/{self._task_parameters.det_type}_calib_max"
                         ][()]
                     except KeyError:
-                        logger.warning('No "Max" powder found in SmallData. Using "Sum" powder.')
+                        logger.warning(
+                            'No "Max" powder found in SmallData. Using "Sum" powder.'
+                        )
                         powder: npt.NDArray[np.float64] = h5[
-                            f"Sums/{self._task_parameters.det_type}_calib"][()]
+                            f"Sums/{self._task_parameters.det_type}_calib"
+                        ][()]
                     if powder.shape != shape:
                         powder: npt.NDArray[np.float64] = np.reshape(powder, shape)
         return powder
