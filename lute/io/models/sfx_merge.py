6a6ed7c41cda30749aecaae9fa073cff8f36aee4
Autoformatter <actions@github.com>
2024-12-04 22:32:51 +0000

    Auto-commit black formatting

--- lute/io/models/sfx_merge.py
+++ lute/io/models/sfx_merge.py
@@ -36,7 +36,7 @@ from lute.io.models.validators import template_parameter_validator
 
 if PYDANTIC_V2:
     # Ignore mypy for now since type checking against pydantic 1.10
-    from pydantic import field_validator # type: ignore
+    from pydantic import field_validator  # type: ignore
 else:
     from pydantic import validator
 
