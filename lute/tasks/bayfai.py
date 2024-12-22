6b3d5c9722c93519d8f5d43382d1238fb0338e1f
LouConreux <louis.conreux1@gmail.com>
2024-12-22 04:35:22 -0800

    [edit] Computation of detector resolution with plots

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -720,12 +720,12 @@ class BayesGeomOpt:
 
         closest_pixel_index = np.argmin(d)
         closest_pixel = d.flatten()[closest_pixel_index]
-        closest_q = (4 * np.pi * np.sin(np.arctan2(closest_pixel / distance)) / self.wavelength) * 1e10
+        closest_q = (4 * np.pi * np.sin(np.arctan2(closest_pixel, distance)) / self.wavelength) * 1e10
         closest_resol = 2 * np.pi / closest_q
 
         furthest_pixel_index = np.argmax(d)
         furthest_pixel = d.flatten()[furthest_pixel_index]
-        furthest_q = (4 * np.pi * np.sin(np.arctan2(furthest_pixel / distance)) / self.wavelength) * 1e10
+        furthest_q = (4 * np.pi * np.sin(np.arctan2(furthest_pixel, distance)) / self.wavelength) * 1e10
         furthest_resol = 2 * np.pi / furthest_q
         
         xmin, xmax = x.min(), x.max()
@@ -736,9 +736,9 @@ class BayesGeomOpt:
         d_top = abs(cy - ymax)
         border_distances = [d_left, d_right, d_bottom, d_top]
         border_pixel = min(border_distances)
-        border_q = (4 * np.pi * np.sin(np.arctan2(border_pixel / distance)) / self.wavelength) * 1e10
+        border_q = (4 * np.pi * np.sin(np.arctan2(border_pixel, distance)) / self.wavelength) * 1e10
         border_resol = 2 * np.pi / border_q
-        border_2_q = (4 * np.pi * np.sin(np.arctan2(border_pixel / 2 * distance)) / self.wavelength) * 1e10
+        border_2_q = (4 * np.pi * np.sin(np.arctan2(border_pixel / 2, distance)) / self.wavelength) * 1e10
         border_2_resol = 2 * np.pi / border_2_q
 
         circle_closest = plt.Circle((cx, cy), closest_pixel, color='green', linestyle='dashed', fill=False)
