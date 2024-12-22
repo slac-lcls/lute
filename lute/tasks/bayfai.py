29d8c1e9fb789cb88f9e38ca87f2545393ebac1a
Autoformatter <actions@github.com>
2024-12-22 13:13:49 +0000

    Auto-commit black formatting

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -745,10 +745,7 @@ class BayesGeomOpt:
         )
         border_resol = 2 * np.pi / border_q
         border_2_q = (
-            4
-            * np.pi
-            * np.sin(np.arctan2(border_pixel / 2, distance))
-            / self.wavelength
+            4 * np.pi * np.sin(np.arctan2(border_pixel / 2, distance)) / self.wavelength
         )
         border_2_resol = 2 * np.pi / border_2_q
 
