731b511179c293779d394f9c5973ec2f3b058966
LouConreux <louis.conreux1@gmail.com>
2025-01-06 05:26:47 -0800

    [edit] Comment animation

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -536,11 +536,9 @@ class BayesGeomOpt:
         )
         self.comm.Barrier()
 
-        self.bayes_opt_animation(
-            results["bo_history"], n_samples, n_iterations, bounds, res, dist
-        )
-
-        self.comm.Barrier()
+        # self.bayes_opt_animation(
+        #     results["bo_history"], n_samples, n_iterations, bounds, res, dist
+        # )
 
         self.scan = {}
         self.scan["bo_history"] = self.comm.gather(results["bo_history"], root=0)
