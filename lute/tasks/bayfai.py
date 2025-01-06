e3eeb2f0a69a11838356da3e640aa3b410f58845
LouConreux <louis.conreux1@gmail.com>
2025-01-06 04:48:26 -0800

    [edit] Redo bayesian opt animation with correction

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -536,6 +536,10 @@ class BayesGeomOpt:
         )
         self.comm.Barrier()
 
+        self.bayes_opt_animation(results["bo_history"], n_samples, n_iterations, bounds, res, dist)
+
+        self.comm.Barrier()
+
         self.scan = {}
         self.scan["bo_history"] = self.comm.gather(results["bo_history"], root=0)
         self.scan["params"] = self.comm.gather(results["params"], root=0)
