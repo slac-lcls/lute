d1d8a5582141959159c0cda7225e137d1f3ca5d5
LouConreux <louis.conreux1@gmail.com>
2025-01-09 02:25:33 -0800

    [edit] Change diagnostic output plot design + animation

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -536,9 +536,9 @@ class BayesGeomOpt:
         )
         self.comm.Barrier()
 
-        # self.bayes_opt_animation(
-        #     results["bo_history"], n_samples, n_iterations, bounds, res, dist
-        # )
+        self.bayes_opt_animation(
+            results["bo_history"], n_samples, n_iterations, bounds, res, dist
+        )
 
         self.scan = {}
         self.scan["bo_history"] = self.comm.gather(results["bo_history"], root=0)
