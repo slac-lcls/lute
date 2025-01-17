8ff6e9cdef55ca923e234a56e6ea6b516da16d3c
Autoformatter <actions@github.com>
2025-01-17 21:49:19 +0000

    Auto-commit black formatting

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -539,7 +539,7 @@ class BayesGeomOpt:
         self.bayes_opt_animation(
             results["bo_history"], n_samples, n_iterations, bounds, res, dist
         )
-        
+
         self.comm.Barrier()
 
         self.scan = {}
