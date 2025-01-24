eaa3dac0cc4e9bc6492ac81eeaf51b61fbe62925
LouConreux <louis.conreux1@gmail.com>
2025-01-24 11:17:16 -0800

    [edit] blablabla

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -1181,6 +1181,7 @@ class OptimizePyFAIGeometry(Task):
                 in_file = pick_template(
                     self._task_parameters.exp, det_type, src, pixel_size_um, self.shape
                 )
+                logger.info(f"Using default geometry file {in_file}")
         else:
             self.pixel_size = self.det.pixel_size(self.ds.env()) * 1e-6
         psana_to_pyfai = PsanaToPyFAI(
