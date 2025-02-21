d6bad51d7d9204b9ff50bc6ce827fa60b9b7d92c
Autoformatter <actions@github.com>
2025-02-21 19:28:50 +0000

    Auto-commit black formatting

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -52,8 +52,8 @@ from mpi4py import MPI
 
 pyFAI.use_opencl = False
 
-os.environ['NUMEXPR_MAX_THREADS'] = '16'
-os.environ['NUMEXPR_NUM_THREADS'] = '16'
+os.environ["NUMEXPR_MAX_THREADS"] = "16"
+os.environ["NUMEXPR_NUM_THREADS"] = "16"
 
 logger: logging.Logger = get_logger(__name__)
 
