b5128421a4973ea2fc883237f14074ab43596954
LouConreux <louis.conreux1@gmail.com>
2025-02-28 14:58:05 -0800

    [edit] Fix error in calib file path selection

--- lute/io/calib.py
+++ lute/io/calib.py
@@ -58,7 +58,8 @@ calib_groups = (
     "Camera::CalibV1",
     "Camera::CalibV1",
 )
-det_names = (
+
+psana_det_names = (
     "UNDEFINED",
     "Cspad",
     "Cspad2x2",
@@ -101,6 +102,49 @@ det_names = (
     "Alvium",
 )
 
+calib_det_names = (
+    "UNDEFINED",
+    "Cspad",
+    "Cspad2x2",
+    "Princeton",
+    "pnCCD",
+    "Tm6740",
+    "Opal1000",
+    "Opal2000",
+    "Opal4000",
+    "Opal8000",
+    "OrcaFl40",
+    "Epix",
+    "Epix10k",
+    "Epix100a",
+    "Fccd960",
+    "Andor",
+    "Acqiris",
+    "Imp",
+    "Quartz4A150",
+    "Rayonix",
+    "Evr",
+    "Fccd",
+    "Timepix",
+    "Fli",
+    "Pimax",
+    "Andor3d",
+    "Jungfrau",
+    "Jungfrau",
+    "Jungfrau",
+    "Zyla",
+    "ControlsCamera",
+    "Epix10ka",
+    "Uxi",
+    "Pixis",
+    "Epix10ka2M",
+    "Epix10kaQuad",
+    "Streak",
+    "Archon",
+    "iStar",
+    "Alvium",
+)
+
 hutches = (
     "UNDEFINED",
     "XPP",
@@ -120,21 +164,22 @@ stations = (
 )
 
 
-det_names_lower = tuple(name.lower() for name in det_names)
-det_lower_to_name = dict(zip(det_names_lower, det_names))
-det_to_group = dict(zip(det_names, calib_groups))
-det_to_group_lower = dict(zip(det_names_lower, calib_groups))
-hutch_to_station = dict(zip(hutches, stations))
+psana_det_names_lower = tuple(name.lower() for name in psana_det_names)
 
+psana_to_calib_det_name = dict(zip(psana_det_names_lower, calib_det_names))
+
+det_to_group = dict(zip(calib_det_names, calib_groups))
+
+hutch_to_station = dict(zip(hutches, stations))
 
 def group_from_det_type(det_type: str) -> str:
     """Retrieve the group string from the detector type."""
     det_type_lower = det_type.lower()
-    group = det_to_group_lower.get(det_type_lower, "NOT IMPLEMENTED")
+    det_name = psana_to_calib_det_name.get(det_type_lower, "NOT IMPLEMENTED")
+    group = det_to_group.get(det_name, "NOT IMPLEMENTED")
     if group == "NOT IMPLEMENTED":
         raise ValueError(f"Unknown detector type: {det_type}")
     return group
-
 
 def source_from_det_info(det_type: str, hutch: str) -> str:
     """Retrieve the source string from the detector type and hutch."""
@@ -145,16 +190,16 @@ def source_from_det_info(det_type: str, hutch: str) -> str:
     if "." in det_type and det_type.split(".")[-1].isdigit():
         det_type, det_id = det_type.rsplit(".", 1)
         det_type_lower = det_type.lower()
-        det = det_lower_to_name.get(det_type_lower, "NOT IMPLEMENTED")
-        if det == "NOT IMPLEMENTED":
+        det_name = psana_to_calib_det_name.get(det_type_lower, "NOT IMPLEMENTED")
+        if det_name == "NOT IMPLEMENTED":
             raise ValueError(f"Unknown detector type: {det_type}")
-        return f"{source_begin}:{det}.{det_id}"
+        return f"{source_begin}:{det_name}.{det_id}"
     else:
         det_type_lower = det_type.lower()
-        det = det_lower_to_name.get(det_type_lower, "NOT IMPLEMENTED")
-        if det == "NOT IMPLEMENTED":
+        det_name = psana_to_calib_det_name.get(det_type_lower, "NOT IMPLEMENTED")
+        if det_name == "NOT IMPLEMENTED":
             raise ValueError(f"Unknown detector type: {det_type}")
-        return f"{source_begin}:{det}.0"
+        return f"{source_begin}:{det_name}.0"
 
 
 def select_calib_file(calib_dir: str, run: int) -> str:
