cbc117077bb4fd8201c723a68b4ec8af97f181b5
LouConreux <louis.conreux1@gmail.com>
2025-03-04 15:51:04 -0800

    [edit] Print in_file after fetching the template for Rayonix

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -84,7 +84,7 @@ class OptimizePyFAIGeometry(Task):
                 )
         else:
             self.pixel_size = self.det.pixel_size(self.ds.env()) * 1e-6
-        print(in_file)
+        logger.info(in_file)
         psana_to_pyfai = PsanaToPyFAI(
             in_file=in_file,
             det_type=det_type,
