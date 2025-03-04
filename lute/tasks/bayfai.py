8076e893190d0ca49a97a0920518251cedbce591
Autoformatter <actions@github.com>
2025-03-04 19:46:30 +0000

    Auto-commit black formatting

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -20,7 +20,7 @@ import os
 import logging
 from typing import Optional, Tuple
 import sys
-from glob import glob # type: ignore
+from glob import glob  # type: ignore
 
 sys.path.append("/sdf/home/l/lconreux/LCLSGeom")
 from LCLSGeom.swap_geom import (  # type: ignore
