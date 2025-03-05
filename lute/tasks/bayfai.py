a966602c19c0e93133e12f57e62454f224ca774e
Autoformatter <actions@github.com>
2025-03-05 01:54:52 +0000

    Auto-commit black formatting

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -241,7 +241,9 @@ class OptimizePyFAIGeometry(Task):
                 else:
                     powder += np.array(batch_powder)
 
-                logger.info(f"Processed {min(idx+batch_size,U.shape[1])} images", flush=True)
+                logger.info(
+                    f"Processed {min(idx+batch_size,U.shape[1])} images", flush=True
+                )
         powder = np.reshape(powder, self.shape)
         return powder
 
