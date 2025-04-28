3e048c776b95063d5ada249458be9fdc927931a0
gadorlhiac <gadorlhiac@gmail.com>
2025-04-28 09:06:21 -0700

    DOC Placeholder documentation for architecture overview.

--- lute/tasks/tasklets.py
+++ lute/tasks/tasklets.py
@@ -57,7 +57,6 @@ __all__ = [
     "indexamajig_summary_indexing_rate",
     "compare_hkl_fom_summary",
     "setup_dimple_uglymol",
-    "setup_smd2_env",
 ]
 __author__ = "Gabriel Dorlhiac"
 
@@ -326,53 +325,3 @@ def setup_dimple_uglymol(
         from lute.tasks.util.html import DIMPLE_HTML
 
         f.write(DIMPLE_HTML)
-
-
-def setup_smd2_env() -> None:
-    """Setup environment variables smalldata_tools uses with psana2."""
-    # partition: str = ...
-    nodes: Optional[str] = os.getenv("SLURM_NNODES")
-    cores_per_node: Optional[str] = os.getenv("SLURM_NTASKS_PER_NODE")
-    # If above are None, not running in SLURM
-    if nodes is None or cores_per_node is None:
-        return
-    mpi_slots: int = int(cores_per_node) * int(nodes) - 1
-    default_srv_cores: int = 16 * int(nodes)
-
-    srv_cores: int = default_srv_cores
-
-    default_eb_cores: int = (mpi_slots - srv_cores) // 16
-    eb_cores: int = default_eb_cores
-
-    os.environ["PS_SRV_NODES"] = str(srv_cores)
-    os.environ["PS_EB_NODES"] = str(eb_cores)
-
-    slurm_job_nodelist: Optional[str] = os.getenv("SLURM_JOB_NODELIST")
-    if slurm_job_nodelist is None:
-        return
-    cmd: List[str] = ["scontrol", "show", "hostnames", slurm_job_nodelist]
-    host_list_bytes: bytes
-    host_list_bytes, _ = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
-
-    host_list: List[str] = host_list_bytes.decode().split("\n")[:-1]
-
-    slurm_job_id: Optional[str] = os.getenv("SLURM_JOB_ID")
-    if slurm_job_id is None:
-        return
-    host_file: str = f"slurm_host_{slurm_job_id}"
-    with open(host_file, "w") as f:
-        for i in range(len(host_list)):
-            if i == 0:
-                f.write(f"{host_list[i]} slots=1")
-            else:
-                f.write(f"{host_list[i]}")
-
-    # cpus_on_node: Optional[str] = os.getenv("SLURM_CPUS_ON_NODE")
-    # Same as cores_per_node above
-    # slurm_job_num_nodes: Optional[str] = os.getenv("SLURM_JOB_NUM_NODES")
-    # Same as nodes as above
-
-    n_ranks: int = int(cores_per_node) * (int(nodes) - 1) + 1
-
-    os.environ["PS_HOST_FILE"] = host_file
-    os.environ["PS_N_RANKS"] = str(n_ranks)
