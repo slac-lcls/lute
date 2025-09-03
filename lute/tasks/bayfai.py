048630f98bf7cf12267ec4ed30dc9e8dfa104e17
Louis Conreux <louis.conreux1@gmail.com>
2025-09-02 17:56:53 -0700

    [edit] BayFAIv2 integrated in lute!

--- lute/tasks/bayfai.py
+++ lute/tasks/bayfai.py
@@ -100,6 +100,7 @@ class OptimizePyFAIGeometry(Task):
                 )
             )
             diagnostics_plot = optimizer.create_diagnostics_panel(
+                history=result['history'],
                 powder=optimizer.powder,
                 detector=calib_detector,
                 distance=params[0],
