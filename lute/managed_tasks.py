ea36534a2198d8622d57a4d9cdc82dcdd279ba5d
kmecseki <kmecseki@slac.stanford.edu>
2025-09-12 17:51:53 -0700

    Changed om source and added more parameters

--- lute/managed_tasks.py
+++ lute/managed_tasks.py
@@ -161,4 +161,4 @@ Xtc1to2Converter: Executor = Executor("ConvertXtc1to2")
 
 CheetahRunner: Executor = Executor("RunCheetah")
 """Run Cheetah task."""
-CheetahRunner.shell_source("/sdf/group/lcls/ds/tools/om/om/om-071725/bin/activate-om")
+CheetahRunner.shell_source("/sdf/group/lcls/ds/tools/om/setup-om.sh")
