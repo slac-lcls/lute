f1dcb8349d22f426bc103d55e24161f49714ba68
Louis Conreux <louis.conreux1@gmail.com>
2025-09-24 11:17:55 -0700

    MNT typo in model

--- lute/io/models/bayfai.py
+++ lute/io/models/bayfai.py
@@ -91,7 +91,7 @@ class BayFAIParameters(TaskParameters):
             "dist": 0.0005,
             "poni1": 0.0001,
             "poni2": 0.0001,
-            "rot1": 0.02
+            "rot1": 0.02,
             "rot2": 0.02,
             "rot3": 0.02,
         },
