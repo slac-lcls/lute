677e2b11647b2d7e7ef0fcb8746c9136199385ae
gadorlhiac <gadorlhiac@gmail.com>
2025-09-29 18:39:57 -0700

    SKL Work in progress for Cheetah based SFX workflow. Add support for compression to Cheetah.

--- lute/io/models/cheetah.py
+++ lute/io/models/cheetah.py
@@ -8,48 +8,21 @@ Classes:
 __all__ = ["RunCheetahParameters"]
 
 
-from pydantic import BaseModel, Field
-from typing import Any, Dict
+from typing import Any, Dict, Literal, Optional
+
+from pydantic import BaseModel, Field, root_validator
+
 from lute.io.models.base import (
     ThirdPartyParameters,
     TemplateConfig,
     validator,
-    Optional,
 )
+from lute.io.models.sfx_find_peaks import SZCompressorParameters
+from lute.io.models.validators import template_parameter_validator
 
 
 class RunCheetahParameters(ThirdPartyParameters):
     """Parameters for running OM Cheetah Task."""
-
-    executable: str = Field(
-        "/sdf/group/lcls/ds/tools/openmpi-5.0.6/bin/mpirun",
-        description="MPI executable.",
-        flag_type="",
-    )
-    n_processes: int = Field(
-        4,
-        description="Number of processes to launch.",
-        flag_type="-",
-        rename_param="n",
-    )
-    om_executable: str = Field(
-        "/sdf/group/lcls/ds/tools/om/om/om-071725/bin/om_monitor.py",
-        description="OM Cheetah binary.",
-        flag_type="",
-    )
-    om_config: str = Field(
-        "/sdf/scratch/users/k/kmecseki/cheetah/test.yaml",
-        description="Config file for OM Cheetah.",
-        flag_type="-",
-        rename_param="c",
-    )
-    lute_template_cfg: TemplateConfig = Field(
-        TemplateConfig(
-            template_name="cheetah_template.yaml",
-            output_path="/sdf/scratch/users/k/kmecseki/cheetah/cheetah_testconfig.yaml",
-        ),
-        description="Template rendering configuration",
-    )
 
     class CheetahSubconfigParameters(BaseModel):
         """Parameters for OM Cheetah itself."""
@@ -64,11 +37,6 @@ class RunCheetahParameters(ThirdPartyParameters):
 
         class DataRetrievalLayerParameters(BaseModel):
             """Parameters for the data retrieving layer."""
-
-            psana_calibration_directory: Optional[str] = Field(
-                "/sdf/data/lcls/ds/xpp/xpptut15/calib/",
-                flag_type="",
-            )
 
             class DataSourcesParameters(BaseModel):
                 """Parameters for the data retrieval layer data sources subfields."""
@@ -85,6 +53,14 @@ class RunCheetahParameters(ThirdPartyParameters):
                     type: Optional[str] = Field("EpicsVariablePsana", flat_type="")
                     psana_name: Optional[str] = Field("detector_z", flat_type="")
                     value: Optional[str] = Field("DetectorDistanceValue", flat_type="")
+
+            psana_calibration_directory: Optional[str] = Field(
+                "/sdf/data/lcls/ds/xpp/xpptut15/calib/",
+                description="Location of the psana1 calibration directory.",
+            )
+            data_sources: Optional[DataSourcesParameters] = Field(
+                None, description="Configuration for data source retrieval."
+            )
 
         class CheetahParameters(BaseModel):
             """Parameters for Cheetah."""
@@ -149,6 +125,95 @@ class RunCheetahParameters(ThirdPartyParameters):
                 800,
                 flag_type="",
             )
+
+        class DataCompressionParameters(BaseModel):
+            run_compression: bool = Field(
+                False,
+                description="Whether to enable compression.",
+            )
+            backend: Optional[Literal["roibinsz"]] = Field(
+                None,
+                description=(
+                    "The compression backend to use. Currently only RoiBinSz "
+                    "with libpressio is available."
+                ),
+            )
+            compression_parameters: Optional[SZCompressorParameters] = Field(
+                None,
+                description="The actual compression parameters. Varies by backend.",
+            )
+
+            @root_validator(pre=False)
+            def check_backend_matches_parameters(
+                cls, values: Dict[str, Any]
+            ) -> Dict[str, Any]:
+                if values["backend"] == "roibinsz":
+                    if not isinstance(
+                        values["compression_parameters"], SZCompressorParameters
+                    ):
+                        raise ValueError(
+                            "For the libpressio RoiBinSz compression backend you must "
+                            "use SZCompressorParameters for `compression_parameters`."
+                        )
+                return values
+
+        om: Optional[OmParameters] = Field(None, description="Global options for OM.")
+        data_retrieval_layer: Optional[DataRetrievalLayerParameters] = Field(
+            None,
+            description="Options for configuring the data retrieval.",
+        )
+        cheetah: Optional[CheetahParameters] = Field(
+            None, description="Cheetah specific parameters (like output directories)."
+        )
+        crystallography: Optional[CrystallographyParameters] = Field(
+            None, description="Generic crystallography parameters, like geometry file."
+        )
+        peakfinder8_peak_detection: Optional[Peakfinder8PeakDetectionParameters] = (
+            Field(
+                None,
+                description="Peakfinder8 peak finding algorithm specific parameters.",
+            )
+        )
+        compression: Optional[DataCompressionParameters] = Field(
+            None, description="Data compression parameters."
+        )
+
+    _set_cheetah_template_parameters = template_parameter_validator("cheetah_subconfig")
+
+    executable: str = Field(
+        "/sdf/group/lcls/ds/tools/openmpi-5.0.6/bin/mpirun",
+        description="MPI executable.",
+        flag_type="",
+    )
+    n_processes: int = Field(
+        4,
+        description="Number of processes to launch.",
+        flag_type="-",
+        rename_param="n",
+    )
+    om_executable: str = Field(
+        "/sdf/group/lcls/ds/tools/om/om/om-071725/bin/om_monitor.py",
+        description="OM Cheetah binary.",
+        flag_type="",
+    )
+    om_config: str = Field(
+        "/sdf/scratch/users/k/kmecseki/cheetah/test.yaml",
+        description="Config file for OM Cheetah.",
+        flag_type="-",
+        rename_param="c",
+    )
+    cheetah_subconfig: Optional[CheetahSubconfigParameters] = Field(
+        None,
+        description="The parameter set for the OM configuration YAML.",
+        flag_type="",  # Does nothing since None by time it's seen by Task
+    )
+    lute_template_cfg: TemplateConfig = Field(
+        TemplateConfig(
+            template_name="cheetah_template.yaml",
+            output_path="/sdf/scratch/users/k/kmecseki/cheetah/cheetah_testconfig.yaml",
+        ),
+        description="Template rendering configuration",
+    )
 
     @validator("lute_template_cfg", always=True)
     def update_output_path(
---
+++ workflows/airflow/cheetah_sfx.py
@@ -0,0 +1,77 @@
+"""Run SFX processing using Cheetah.
+
+Note:
+    The task_id MUST match the managed task name when defining DAGs - it is used
+    by the operator to properly launch it.
+    dag_id names must be unique, and they are not namespaced via folder
+    hierarchy. I.e. all DAGs on an Airflow instance must have unique ids. The
+    Airflow instance used by LUTE is currently shared by other software - DAG
+    IDs should always be prefixed with `lute_`. LUTE scripts should append this
+    internally, so a DAG "lute_test" can be triggered by asking for "test"
+"""
+
+import os
+from datetime import datetime
+from typing import Any, Dict
+
+from airflow import DAG
+from airflow.decorators import task
+
+from lute.operators.jidoperators import JIDSlurmOperator
+
+dag_id: str = f"lute_{os.path.splitext(os.path.basename(__file__))[0]}"
+description: str = "Run Cheetah SFX."
+
+dag: DAG = DAG(
+    dag_id=dag_id,
+    start_date=datetime(2000, 4, 10),
+    schedule_interval=None,
+    description=description,
+    is_paused_upon_creation=False,
+)
+
+peak_finder: JIDSlurmOperator = JIDSlurmOperator(
+    task_id="CheetahRunner", dag=dag, trigger_rule="none_failed"
+)
+
+indexer: JIDSlurmOperator = JIDSlurmOperator(
+    max_cores=120,
+    max_nodes=1,
+    task_id="CrystFELIndexer",
+    dag=dag,
+    trigger_rule="none_failed",
+)
+
+concatenator: JIDSlurmOperator = JIDSlurmOperator(
+    max_cores=2, task_id="StreamFileConcatenator", dag=dag, trigger_rule="none_failed"
+)
+
+# Merge
+merger: JIDSlurmOperator = JIDSlurmOperator(
+    max_cores=120,
+    max_nodes=1,
+    task_id="PartialatorMerger",
+    dag=dag,
+    trigger_rule="none_failed",
+)
+
+# Figures of merit
+hkl_comparer: JIDSlurmOperator = JIDSlurmOperator(
+    max_cores=8, max_nodes=1, task_id="HKLComparer", dag=dag, trigger_rule="none_failed"
+)
+
+# HKL conversions
+hkl_manipulator: JIDSlurmOperator = JIDSlurmOperator(
+    max_cores=8,
+    max_nodes=1,
+    task_id="HKLManipulator",
+    dag=dag,
+    trigger_rule="none_failed",
+)
+
+# CCP4
+dimple_runner: JIDSlurmOperator = JIDSlurmOperator(task_id="DimpleSolver", dag=dag)
+
+# Branch Workflow depending on available psana version
+peak_finder >> indexer >> concatenator >> merger >> hkl_manipulator >> dimple_runner
+merger >> hkl_comparer
