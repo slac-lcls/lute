b53cdaf5c46a813f5add7824b69dc58029c07fc2
Valerio Mariani <valmar@slac.stanford.edu>
2025-12-08 12:52:28 -0800

    Add SmallDataXpp task for XPP data reduction work

--- lute/managed_tasks.py
+++ lute/managed_tasks.py
@@ -76,6 +76,20 @@ SmallDataProducerSpack.add_tasklet(
 )
 SmallDataProducerSpack.update_environment(setup_smd2_env)
 
+SmallDataProducerXpp: Executor = Executor("SubmitSMD")
+"""Runs the production of a LCLS2 smalldata HDF5 file using the spack environment."""
+SmallDataProducerXpp.shell_source(
+    "/sdf/group/lcls/ds/ana/sw/conda2/manage/bin/xpp_drp_gpu.sh"
+)
+SmallDataProducerXpp.add_tasklet(
+    clone_smalldata,
+    ["{{ producer }}"],
+    when="before",
+    set_result=False,
+    set_summary=False,
+)
+SmallDataProducerXpp.update_environment(setup_smd2_env)
+
 SmallDataXSSAnalyzer: MPIExecutor = MPIExecutor("AnalyzeSmallDataXSS")
 """Process scattering results from a Small Data HDF5 file."""
 
